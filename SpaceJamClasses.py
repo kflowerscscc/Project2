cf8fd9c86348d29eb5f30f76f32eda43948c99cb
kloesch166 <kloesch@student.cscc.edu>
2024-02-21 16:42:56 -0500

    Refactor code
    Refactor SpaceJam classes to use GameObject class.
    Update textures.
    Change scale to be a variable instead of hardcoded.
    I

--- SpaceJamClasses.py
+++ SpaceJamClasses.py
@@ -7,7 +7,7 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
 
 
-class Planet(ShowBase):
+class GameObject(ShowBase):
     
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, textPath:str, posVec: Vec3, scaleVec:float):
         self.modelNode = loader.loadModel(modelPath)
@@ -17,13 +17,33 @@ class Planet(ShowBase):
         self.modelNode.setName(nodeName)
         tex = loader.loadTexture(textPath)
         self.modelNode.setTexture(tex, 1)
-       
 
+class Drone(GameObject):
+    
+    droneCount = 0
+
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, textPath:str, posVec: Vec3, scaleVec:float):
+        super().__init__(loader, modelPath, parentNode, nodeName, textPath, posVec, scaleVec)
+
+
+#  Code below was refactored using class inheritance.
+'''
 class Drone(ShowBase):
 
     # How many drones have been spawned
     droneCount = 0
 
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, textPath:str, posVec: Vec3, scaleVec:float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(textPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Planet(ShowBase):
+    
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, textPath:str, posVec: Vec3, scaleVec:float):
         self.modelNode = loader.loadModel(modelPath)
         self.modelNode.reparentTo(parentNode)
@@ -65,3 +85,4 @@ class Universe(ShowBase):
         self.modelNode.setName(nodeName)
         tex = loader.loadTexture(textPath)
         self.modelNode.setTexture(tex, 1)
+'''
--- Spacejam.py
+++ Spacejam.py
@@ -1,5 +1,5 @@
 # Author: Kristi Loesch
-# Date: 2/14/2024
+# Date: 2/21/2024
 # Description: Project 2
 
 
@@ -15,56 +15,55 @@ class SpaceJam(ShowBase):
   
         ShowBase.__init__(self)
 
-        self.Universe = spaceJamClasses.Universe(self.loader, "./Assets/Universe/Universe.x", self.render, 'Universe', "./Assets/Universe/starfield-in-blue.jpg", (0,0,0), 15000 )
-        self.Planet1 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet1', "./Assets/Planets/jupiter.jpg", Vec3(-6000,-3000,-800), 250 )
-        self.Planet2 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet2', "./Assets/Planets/mars.jpg", Vec3(0,6000,0), 300 )
-        self.Planet3 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet3', "./Assets/Planets/mars.jpg", Vec3(500,-5000), 200 )
-        self.Planet3 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet3', "./Assets/Planets/earth_daymap.jpg", Vec3(300,600,500), 150)
-        self.Planet4 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet4', "./Assets/Planets/cracked_earth.jpg", Vec3(300,6000,500), 150 )
-        self.Planet5 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet5', "./Assets/Planets/mars.jpg", Vec3(700,-2000,100), 500 )
-        self.Planet6 = spaceJamClasses.Planet(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet6', "./Assets/Planets/mars.jpg", Vec3(0,-9000,-1400), 700 )
-        self.SpaceStation1 = spaceJamClasses.SpaceStation(self.loader, "./Assets/Space Station/spaceStation.egg", self.render, 'Space Station', "./Assets/Space Station/SpaceStation1_Dif2.png", (1500,1000,-100), 40 )
-        self.Player1 = spaceJamClasses.Player(self.loader, "./Assets/Spaceships/theBorg.x", self.render, 'Player', "./Assets/Spaceships/theBorg.jpg", (0,0,0), 5 )
+        self.Universe = spaceJamClasses.GameObject(self.loader, "./Assets/Universe/Universe.x", self.render, 'Universe', "./Assets/Universe/starfield-in-blue.jpg", (0,0,0), 15000 )
+        self.Planet1 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet1', "./Assets/Planets/jupiter.jpg", Vec3(-6000,-3000,-800), 250 )
+        self.Planet2 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet2', "./Assets/Planets/earth_daymap.jpg", Vec3(0,6000,0), 300 )
+        self.Planet3 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet3', "./Assets/Planets/saturn.jpg", Vec3(300,600,500), 150)
+        self.Planet4 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet4', "./Assets/Planets/cracked_earth.jpg", Vec3(300,3000,500), 150 )
+        self.Planet5 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet5', "./Assets/Planets/venus.jpg", Vec3(700,-2000,100), 500 )
+        self.Planet6 = spaceJamClasses.GameObject(self.loader, "./Assets/Planets/protoPlanet.x", self.render, 'Planet6', "./Assets/Planets/pluto.jpg", Vec3(0,-9000,-1400), 700 )
+        self.SpaceStation1 = spaceJamClasses.GameObject(self.loader, "./Assets/Space Station/spaceStation.egg", self.render, 'Space Station', "./Assets/Space Station/SpaceStation1_Dif2.png", (1500,1000,-100), 40 )
+        self.Player1 = spaceJamClasses.GameObject(self.loader, "./Assets/Spaceships/theBorg.x", self.render, 'Player', "./Assets/Spaceships/theBorg.jpg", (0,0,0), 5 )
         
 
-        # Place drones cloud around an object
+        # Place drone cloud around an object
         fullCycle = 60
         for j in range(fullCycle):
             spaceJamClasses.Drone.droneCount += 1
-            nickName = "Drone" + str(spaceJamClasses.Drone.droneCount)
-            self.DrawCloudDefense(self.Planet3, nickName )
+            nickName = "Drone" + str(spaceJamClasses.Drone.droneCount) 
+            self.DrawCloudDefense(self.Planet3, nickName, 300 ) # Call function to draw cloud,  indicate scale
 
         
         # Place baseball seams around an object
-        seamCycle = 150
-        for j in range(seamCycle):
+        steps = 150
+        for step in range(steps):
             spaceJamClasses.Drone.droneCount += 1
             droneName = "Seam" + str(spaceJamClasses.Drone.droneCount)
-            self.DrawBaseballSeams(self.Planet4, droneName, j, 50 )
+            self.DrawBaseballSeams(self.Planet4, droneName, step, 50, 300) # Call function to draw baseball seams, indicate scale
 
-        x=0
-
+        
         # Draw circle on each plane around an object
+        x=0
         for i in range(100):
             theta = x
             spaceJamClasses.Drone.droneCount += 1
             droneName = "Circle" + str(spaceJamClasses.Drone.droneCount)
-            self.DrawCircleXY(self.Planet2, droneName, theta, 450)
-            self.DrawCircleXZ(self.Planet2, droneName, theta, 450)
-            self.DrawCircleYZ(self.Planet2, droneName, theta, 450)
+            self.DrawCircleXY(self.Planet2, droneName, theta, 450) # Call function to draw XY circle, indicate scale
+            self.DrawCircleXZ(self.Planet2, droneName, theta, 450) # Call function to draw XZ circle, indicate scale
+            self.DrawCircleYZ(self.Planet2, droneName, theta, 450)# Call function to draw YZ circle, indicate scale
             x = x + .0628
         
 
-    def DrawCloudDefense(self, centralObject, nickname):
+    def DrawCloudDefense(self, centralObject, nickname, scale):
         unitVec = defensePaths.Cloud()
         unitVec.normalize()
-        position = unitVec * 500 + centralObject.modelNode.getPos()
+        position = unitVec * scale + centralObject.modelNode.getPos()
         spaceJamClasses.Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, nickname, "./Assets/DroneDefender/octotoad1_auv.png", position, 10)
     
-    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius=1):
+    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, scale, radius=1):
         unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)
         unitVec.normalize()
-        position = unitVec * 300 + centralObject.modelNode.getPos()
+        position = unitVec * scale + centralObject.modelNode.getPos()
         spaceJamClasses.Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
 
     def DrawCircleXY(self, centralObject, droneName, theta, scale):
@@ -78,7 +77,6 @@ class SpaceJam(ShowBase):
         unitVec.normalize()
         position = unitVec * scale + centralObject.modelNode.getPos()
         spaceJamClasses.Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/color_green.jpg", position, 5)
-
     
     def DrawCircleYZ(self, centralObject, droneName, theta, scale):
         unitVec = defensePaths.CircleYZ(theta)
